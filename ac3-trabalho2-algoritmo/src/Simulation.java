b67a9bb8b98124ec59618c758e8723e9942c003b
aray4mikael <mikael.araya9500@gmail.com>
2022-05-23 22:22:34 -0300

    comentários final

--- ac3-trabalho2-algoritmo/src/Simulation.java
+++ ac3-trabalho2-algoritmo/src/Simulation.java
@@ -12,7 +12,10 @@ public class Simulation {
     static void next(ArrayList<String> input, InstructionStatus instructionStatus, ReservationStations reservationStations,
             RegisterStatus registerStatus) {
 
-               
+    // teste de conflito e renomeação (fazer por ultimo)
+    // dar split no input
+    // testar se estação de reserva está livre
+    // inserir na estação de reserva caso esteja livre
 
                
         reservationStations.getBusy(0);
